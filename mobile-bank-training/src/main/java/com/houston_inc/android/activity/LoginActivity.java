99bc9464a060205ff7d13d99f93926ddc4522ed8
Mikko Vestola <mikko.vestola@iki.fi>
2012-12-04 15:03:08 +0200

    Added click annotations.

--- mobile-bank-training/src/main/java/com/houston_inc/android/activity/LoginActivity.java
+++ mobile-bank-training/src/main/java/com/houston_inc/android/activity/LoginActivity.java
@@ -1,12 +1,26 @@
 package com.houston_inc.android.activity;
 
 import android.app.Activity;
+import android.widget.EditText;
+import android.widget.Toast;
+import com.googlecode.androidannotations.annotations.Click;
 import com.googlecode.androidannotations.annotations.EActivity;
+import com.googlecode.androidannotations.annotations.ViewById;
 import com.houston_inc.android.R;
 
 @EActivity(R.layout.login)
 public class LoginActivity extends Activity {
 
+    @ViewById(R.id.username)
+    EditText usernameField;
+
+    @ViewById(R.id.password)
+    EditText passwordField;
+
+    @Click(R.id.loginButton)
+    void onLoginButtonClick() {
+        Toast.makeText(this, "Button clicked", Toast.LENGTH_LONG).show();
+    }
 
 
 }
